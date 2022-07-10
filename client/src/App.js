a69c7018632a513a7047d524c5b28e5bee332331
Himanshu <himanshumittal4121@gmail.com>
2022-07-11 00:10:19 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+
+
+    <GlobalProvider>
+      <div className='container-main'>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />        
+        <TransactionList />        
+        <AddTransaction />        
+      </div>
+      </div>
+    </GlobalProvider>
+  
+  );
+}
+
+export default App;
---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=> {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        })
+
+    console.log(`MonogoDB Connected: ${conn.connection.host}`.bold.cyan)
+
+    }catch(err){
+
+        console.log(`Error: ${err.message}`.red)
+        process.exit(1);
+    }
+
+}
+
+module.exports = connectDB;
